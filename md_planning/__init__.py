83172158f47c4c53c5685159e616b1b89af0a949
Martin Teller <martin@l1nx.it>
2022-09-20 14:59:31 +0200

    initial commit

---
+++ md_planning/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for md-planning."""
+
+__author__ = """Martin Teller"""
+__email__ = 'martin@l1nx.it'
+__version__ = '0.1.0'
+
+from .md_planning import ProjectReader, PertDrawer, GanttDrawer
---
+++ setup.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open("README.rst") as readme_file:
+    readme = readme_file.read()
+
+with open("HISTORY.rst") as history_file:
+    history = history_file.read()
+
+requirements = [
+    "Click>=7.0",
+    "pyyaml==6.0",
+    "python-gantt==0.6.0",
+    "python-gantt-csv==0.4.0",
+    "pertchart==0.5.1",
+    "criticalpath==0.1.5",
+    "networkx==2.8.4",
+    "pint==0.19.2",
+    "pandas==1.4.3",
+    "tabulate==0.8.10",
+    "matplotlib==3.6.0",
+]
+
+test_requirements = [
+    "pytest>=3",
+]
+
+setup(
+    author="Martin Teller",
+    author_email="martin@l1nx.it",
+    python_requires=">=3.7",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    description="package to help make yaml based projects with "
+    "markdown-preview-enhanced and vscode",
+    entry_points={
+        "console_scripts": [
+            "md_planning=md_planning.cli:main",
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + "\n\n" + history,
+    include_package_data=True,
+    keywords="md_planning",
+    name="md_planning",
+    packages=find_packages(include=["md_planning", "md_planning.*"]),
+    test_suite="tests",
+    tests_require=test_requirements,
+    url="https://github.com/mtllr/md_planning",
+    version="0.1.0",
+    zip_safe=False,
+)
