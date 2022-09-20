29d955544cd9395c469911b099d899fe8b0877f0
Martin Teller <martin@l1nx.it>
2022-09-20 21:31:18 +0200

    config change

--- md_planning/__init__.py
+++ md_planning/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for md-planning."""
 
 __author__ = """Martin Teller"""
-__email__ = 'martin@l1nx.it'
+__email__ = 'martintellerpro@gmail.com'
 __version__ = '0.1.1'
 
 from .md_planning import ProjectReader, PertDrawer, GanttDrawer
